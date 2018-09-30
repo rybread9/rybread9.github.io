58347ef44c5586e230931d5d822caf4c18e5f8cf
Rylen del Rio <rylendelrio@Rylens-MacBook-Pro.local>
2018-09-29 18:37:34 -0700

    Token is moved by a button

--- trouble_app/app.js
+++ trouble_app/app.js
@@ -6,7 +6,6 @@
 // 3. Player 2 rolls dice.
 // 4. Player 2 moves amount of squares rolled.
 // 5. Repeat until all tokens of one player are in finish div.--> pop up message of "player wins"
-      // add pt to score when a player reaches FINISH. first one to 3 pts wins
 // 6. Token can only go around board once
 
 
@@ -22,9 +21,6 @@
 
 
 $(()=>{
-
-
-
 
 
   // ---GAME RULES MODAL---
@@ -58,29 +54,32 @@ $(()=>{
 
   const rollDice = () => {
     const resultOfRoll = $('#diceRoll').text(Math.floor(Math.random() * 6) +1);
+
+    const movePieceTo = path[newPathValue].append('<div class="halo"></div>');
   }
 
-  let $clickRoll = $('#dice').on('click', rollDice);
+  $('#dice').on('click', rollDice);
 
 // ---MOVING A TOKEN ACCORDING TO DICE ROLL---INCOMPLETE
-
-  const findNewPinkPathValue = () => {
-    const diceRoll = $('#diceRoll').val();//result of dice roll
-    console.log(diceRoll);
-    const currentPathValue = $('#pinkToken').parent().attr('path');
-    console.log(currentPathValue);
-    const newPathValue = parseInt(diceRoll) + parseInt(currentPathValue);
-    console.log(newPathValue);
+  let newPathValue;
+  const $pinkToken = $('<div id="pinkToken"></div>');
+  const $yellowToken = $('<div id="yellowToken"></div>');
+  const findNewPathValue = () => {
+    let diceRoll = $('#diceRoll').val();//result of dice roll
+    if(turn === true) {
+      sendHome();
+      let currentPinkPathValue = $('#pinkToken').parent().attr('path');//current location of pink token
+      let newPathValue = parseInt(diceRoll) + parseInt(currentPinkPathValue);//new location for token
+      const movePieceTo = path[newPathValue].append($pinkToken);// move token to it's new div by accessing path array
+    } else if (turn === false) {
+      sendHome();
+      let currentYellowPathValue = $('#yellowToken').parent().attr('path');//get current location of token
+      let newPathValue = parseInt(diceRoll) + parseInt(currentYellowPathValue);//add them together to get new location for token
+      const movePieceTo = path[newPathValue].append($pinkToken);// move token to it's new div by accessing path array
+    }
   }
 
-  const findNewYellowPathValue = () => {
-    const diceRoll = $('#diceRoll').val();//result of dice roll
-    console.log(diceRoll);
-    const currentPathValue = $('#yellowToken').parent().attr('path');
-    console.log(currentPathValue);
-    const newPathValue = parseInt(diceRoll) + parseInt(currentPathValue);
-    console.log(newPathValue);
-  }
+  $('#move').on('click', findNewPathValue);
 
 
   // ---WIN---
@@ -98,28 +97,20 @@ $(()=>{
 
   let turn = true;
 
-  const $pinkToken = $('<div id="pinkToken"></div>');
-  const $yellowToken = $('<div id="yellowToken"></div>');
+
 
   //when user clicks on a square, alternate between displaying pink and yellow
-  const play = (event) => {
-    const $move = $(event.currentTarget);
-
+  const sendHome = () => {
     //when turn is true, its pink's turn
     if(turn === true){
-
-
       //if you land on an opponent's token, opponent gets sent home
-      if ($(event.currentTarget).hasClass('yellowToken')) {
+      if ($('.gameSquare').hasClass('yellowToken')) {
         $('#yellowToken').appendTo('#box39');
         $('.gameSquares').removeClass('yellowToken');
         $('.gameSquares').removeClass('pinkToken');
         alert(`Womp womp. Yellow has been sent back to Home.`);
       }
-      // when a gameboard div is clicked, a pink token with a class of .pinkToken will be appended to it
-
-
-
+    // when a gameboard div is clicked, a pink token with a class of .pinkToken will be appended to it
     $('.gameSquares').removeClass('pinkToken');
     $move.append($pinkToken);//.attr('path', newPathValue)
     $move.addClass('pinkToken');
@@ -129,7 +120,6 @@ $(()=>{
 
     toggle(turn);
 
-      // $('input').attr('checked', '');
 
     } else if(turn === false) {//when turn = false, its yellow's turn
 
@@ -140,19 +130,17 @@ $(()=>{
         $('.gameSquares').removeClass('yellowToken');
         alert(`Womp womp. Pink has been sent back to Home.`);
       }
-      // when a gameboard div is clicked, a yellow token will be appended to it
+    // when a gameboard div is clicked, a yellow token will be appended to it
+    // console.log(findNewYellowPathValue());
+    $('div').removeClass('yellowToken');
+    $move.append($yellowToken);
+    $move.addClass('yellowToken');
+    checkWin();
+    turn = true;
 
+    toggle(turn);
 
-
-      $('div').removeClass('yellowToken');
-      $move.append($yellowToken);
-      $move.addClass('yellowToken');
-      checkWin();
-      turn = true;
-
-      toggle(turn);
-
-      }
+    }
   }
 
 
@@ -176,6 +164,14 @@ const toggle = (turn) => {
   }
 }
 
+let path = [];
+
+// let path = [
+//   {
+//     id: #box39,
+//     path: 0
+//   },
+// ];
 
 // ---MAKE THE GAMEBOARD A GRID---
 const generateGameBoard = () => {
@@ -190,41 +186,41 @@ const generateGameBoard = () => {
   $('#box41').text('finish');
   $pinkToken.appendTo('#box39');
   $yellowToken.appendTo('#box39');
-  $('#box39').attr('path', 0).on('click', play);
-  $('#box48').attr('path', 1).on('click', play);
-  $('#box57').attr('path', 2).on('click', play);
-  $('#box66').attr('path', 3).on('click', play);
-  $('#box65').attr('path', 4).on('click', play);
-  $('#box64').attr('path', 5).on('click', play);
-  $('#box55').attr('path', 6).on('click', play);
-  $('#box46').attr('path', 7).on('click', play);
-  $('#box37').attr('path', 8).on('click', play);
-  $('#box28').attr('path', 9).on('click', play);
-  $('#box19').attr('path', 10).on('click', play);
-  $('#box10').attr('path', 11).on('click', play);
-  $('#box11').attr('path', 12).on('click', play);
-  $('#box12').attr('path', 13).on('click', play);
-  $('#box13').attr('path', 14).on('click', play);
-  $('#box14').attr('path', 15).on('click', play);
-  $('#box15').attr('path', 16).on('click', play);
-  $('#box16').attr('path', 17).on('click', play);
-  $('#box25').attr('path', 18).on('click', play);
-  $('#box34').attr('path', 19).on('click', play);
-  $('#box43').attr('path', 20).on('click', play);
-  $('#box52').attr('path', 21).on('click', play);
-  $('#box61').attr('path', 22).on('click', play);
-  $('#box70').attr('path', 23).on('click', play);
-  $('#box69').attr('path', 24).on('click', play);
-  $('#box68').attr('path', 25).on('click', play);
-  $('#box59').attr('path', 26).on('click', play);
-  $('#box50').attr('path', 27).on('click', play);
-  $('#box41').attr('path', 28).on('click', play);
+  path.push($('#box39').attr('path', 0));
+  path.push($('#box48').attr('path', 1));
+  path.push($('#box57').attr('path', 2));
+  path.push($('#box66').attr('path', 3));
+  path.push($('#box65').attr('path', 4));
+  path.push($('#box64').attr('path', 5));
+  path.push($('#box55').attr('path', 6));
+  path.push($('#box46').attr('path', 7));
+  path.push($('#box37').attr('path', 8));
+  path.push($('#box28').attr('path', 9));
+  path.push($('#box19').attr('path', 10));
+  path.push($('#box10').attr('path', 11));
+  path.push($('#box11').attr('path', 12));
+  path.push($('#box12').attr('path', 13));
+  path.push($('#box13').attr('path', 14));
+  path.push($('#box14').attr('path', 15));
+  path.push($('#box15').attr('path', 16));
+  path.push($('#box16').attr('path', 17));
+  path.push($('#box25').attr('path', 18));
+  path.push($('#box34').attr('path', 19));
+  path.push($('#box43').attr('path', 20));
+  path.push($('#box52').attr('path', 21));
+  path.push($('#box61').attr('path', 22));
+  path.push($('#box70').attr('path', 23));
+  path.push($('#box69').attr('path', 24));
+  path.push($('#box68').attr('path', 25));
+  path.push($('#box59').attr('path', 26));
+  path.push($('#box50').attr('path', 27));
+  path.push($('#box41').attr('path', 28));
 }
 generateGameBoard();
+console.log(path[2]);
 
 });
-// re-label path-done
-// set gamepath as an array of box#
+// --FOR FUTURE UPDATES--
 // set value of each token as 0
 // each time you roll the dice, you take the dice value and add it to the current value of the token to equal the game path place
 // move the token to the value of the box on Path
